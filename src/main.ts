5ce4aa74fdcf5450466193939e7bde8229e382ff
pereira frédéric <56920249+pereirafrederic-wip@users.noreply.github.com>
2020-01-06 12:22:51 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {GlobalMiddleware } from './middleware/global.middleware'
+
+//middleware
+import { GlobalMiddleware } from './middleware/global.middleware';
+
+//swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //middleware
   app.use(GlobalMiddleware);
+
+  const options = new DocumentBuilder()
+    .setTitle('Users example')
+    .setDescription('The Users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
