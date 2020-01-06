43d01d9257a7daba657fdad78a30be665b0b8679
pereira frédéric <56920249+pereirafrederic-wip@users.noreply.github.com>
2020-01-06 12:38:41 +0100

    service async

--- src/users/db/create-user.dto.ts
+++ src/users/db/create-user.dto.ts
@@ -1,14 +1,18 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsString()
+  @ApiModelProperty()
   readonly id: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly nom: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly prenom: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,36 +6,49 @@ import {
   Body,
   Param,
   Delete,
+  HttpStatus,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { IUsers } from './db/IUsers';
+
+//swagger
+import { ApiUseTags, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiUseTags('todos')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll(): Array<IUsers> {
-    return this.usersService.findAll();
+  public async findAll(@Response() res) {
+    const users = await this.usersService.findAll();
+    return res.status(HttpStatus.OK).json(users);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): IUsers {
-    return this.usersService.findOne(id);
+  public async findOne(@Param() param, @Response() res) {
+    const user = await this.usersService.findById(param.id);
+    return res.status(HttpStatus.OK).json(user);
   }
 
   @Post()
-  create(@Body() user: CreateUserDto): IUsers {
-    return this.usersService.create(user);
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  public async create(@Body() user: CreateUserDto, @Response() res) {
+    const userCreated = await this.usersService.create(user);
+    return res.status(HttpStatus.OK).json(userCreated);
   }
 
   @Put()
-  update(@Body() user: CreateUserDto): IUsers {
-    return this.usersService.update(user);
+  public async update(@Body() user: CreateUserDto, @Response() res) {
+    const userUpdated = await this.usersService.update(user);
+    return res.status(HttpStatus.OK).json(userUpdated);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): string {
-    return this.usersService.delete(id);
+  public async delete(@Param() param, @Response() res) {
+    const string = await this.usersService.delete(param.id);
+    return res.status(HttpStatus.OK).json(string);
   }
 }
